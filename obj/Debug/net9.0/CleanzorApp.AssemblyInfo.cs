f961a6dd882dd454e92e866c37262890027bf6cf
John Carl Fetalver Ola <johncarl4102@gmail.com>
2025-10-22 22:29:00 +0800

    done until email verfication

--- obj/Debug/net9.0/CleanzorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CleanzorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanzorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e7b324c0dee4140a195c0ac585a8b719608186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189e109770ce60acb8f7a4ebcf3c77f8896c773a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanzorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanzorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/interOperation.js
+++ wwwroot/js/interOperation.js
@@ -1,17 +1,41 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAuth, sendSignInLinkToEmail, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const actionCodeSettings = {
+  url: 'http://localhost:5209',
+  handleCodeInApp: true,
+  iOS: {
+    bundleId: 'com.example.ios'
+  },
+  android: {
+    packageName: 'com.example.android',
+    installApp: true,
+    minimumVersion: '12'
+  }
+};
 
 export async function signupUser(email, password) {
     const auth = getAuth();
 
     try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
+        await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+
+        window.localStorage.setItem('emailForSignIn', email);
+
         return {
-            Success: true, Uid: user.uid, ErrorCode: null, ErrorMessage: null
+            Success: true,
+            Message: "Verification link sent to your email. Please check your inbox to complete registration.",
+            Uid: null,
+            ErrorCode: null,
+            ErrorMessage: null
         };
+
     } catch (error) {
         return {
-            Success: false, Uid: null, ErrorCode: error.code, ErrorMessage: error.message
+            Success: false,
+            Message: null,
+            Uid: null,
+            ErrorCode: error.code,
+            ErrorMessage: error.message
         };
     }
 }
@@ -31,4 +55,5 @@ export async function loginUser(email, password) {
         };
     }
 }
+
 window.signupUser = signupUser;
