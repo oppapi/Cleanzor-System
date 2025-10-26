3786a345c15b9992de2fe01375966f743022212d
John Carl Fetalver Ola <johncarl4102@gmail.com>
2025-10-26 23:15:47 +0800

    Add firestore

--- wwwroot/js/interOperation.js
+++ wwwroot/js/interOperation.js
@@ -41,6 +41,52 @@ export async function loginUser(email, password) {
   }
 }
 
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+export async function registerUser(fullname, email, username, schedule) {
+  const auth = getAuth(window.firebaseApp);
+  const db = getFirestore(window.firebaseApp);
+  const user = auth.currentUser; // get logged-in user
+
+  if (!user) {
+    return { Success: false, ErrorCode: "auth/no-current-user" };
+  }
+
+  try {
+    await setDoc(doc(db, "CleanzorUsers", user.uid), {
+      fullname,
+      email,
+      username,
+      schedule,
+      createdAt: new Date()
+    });
+
+    return { Success: true, ErrorCode: null };
+  } catch (error) {
+    return { Success: false, ErrorCode: error.code || "unknown-error" };
+  }
+}
+
+
+//for REMEMBER ME
+export async function rememberLoginUser(email, password) {
+  const auth = getAuth(window.firebaseApp);
+
+  try {
+    await setPersistence(auth, browserLocalPersistence);
+
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    if (!user.emailVerified) {
+      return { Success: false, ErrorCode: "auth/invalid-email-verified" };
+    }
+
+    return { Success: true, ErrorCode: null };} catch (error) {
+    return { Success: false, ErrorCode: error.code || "unknown-error" };
+  }
+}
+
 window.signOutFirebase = async function () {
     const auth = getAuth(window.firebaseApp);
     await signOut(auth);
@@ -55,5 +101,7 @@ window.getFirebaseUserEmail = () => {
     });
 };
 
+
 window.signupUser = signupUser;
 window.loginUser = loginUser;
+window.registerUser = registerUser;
