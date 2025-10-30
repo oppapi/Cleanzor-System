66b72125609a94a7a854ff85c9f8d4a67f886b7b
John Carl Fetalver Ola <johncarl4102@gmail.com>
2025-10-31 02:31:53 +0800

    tried line graph

--- obj/Debug/net9.0/CleanzorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CleanzorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanzorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0608a5f9b0def710431255797cb875fc29f788d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae990848a82480fc121184820431f19e8fb03aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanzorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanzorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
